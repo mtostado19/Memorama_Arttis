7b8467f317fb42b4543712f5aeb3d31237a63493
miguel tostado <mtalamo2000@gmail.com>
2021-12-17 21:10:50 -0600

    Primera version main activity. Firebase conectado

--- app/src/main/java/com/example/memorama_arttis/MainActivity.kt
+++ app/src/main/java/com/example/memorama_arttis/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.memorama_arttis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseReference
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btn_crear : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_crear = findViewById(R.id.btn_crearPartidas);
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        btn_crear.setOnClickListener {
+            myRef.setValue("Hello, World!");
+            val mySnackbar = Snackbar.make(it,
+                "hola", 30000)
+            mySnackbar.setAction("regresar") { MyUndoListener() }
+            mySnackbar.show()
+        }
+    }
+    fun MyUndoListener() {
+        print("f")
     }
 }
